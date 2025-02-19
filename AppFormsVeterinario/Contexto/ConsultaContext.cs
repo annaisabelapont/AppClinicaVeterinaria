f6b629b8b9aed7d1ed9dc637314e8f6b7116d943
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-02-18 23:51:07 -0400

    feat: ConsultaContext

--- AppFormsVeterinario/Contexto/ConsultaContext.cs
+++ AppFormsVeterinario/Contexto/ConsultaContext.cs
@@ -17,7 +17,7 @@ namespace AppFormsVeterinario.Contexto
             conexao = new MySqlConnection(dados_conexao);
         }
 
-        public void CadastrarConsulta(Consulta consulta)
+        public void InserirConsulta(Consulta consulta)
         {
             try
             {
@@ -43,17 +43,103 @@ namespace AppFormsVeterinario.Contexto
                 conexao.Close();
             }
         }
-        public void EditarConsulta()
+        public void AtualizarConsulta(Consulta consulta)
         {
+            try
+            {
+                conexao.Open();
 
+                string sql = "update consulta set tipo = @tipo, dataConsulta = @dataConsulta, idVeterinarioFk = @idVeterinarioFk, idAnimalFk = @idAnimalFk where id = @id";
+
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+                comando.Parameters.AddWithValue("@tipo", consulta.Tipo);
+                comando.Parameters.AddWithValue("@dataConsulta", consulta.DataConsulta);
+                comando.Parameters.AddWithValue("@idVeterinarioFk", consulta.IdVeterinarioFk);
+                comando.Parameters.AddWithValue("@idAnimalFk", consulta.IdAnimalFk);
+
+                int linhasAfetadas = comando.ExecuteNonQuery();
+
+                if (linhasAfetadas > 0)
+                    MessageBox.Show("Consulta atualizada com sucesso!");
+
+                else
+                    MessageBox.Show("Nenhum registro foi atualizado. Verifique o ID informado.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
         }
-        public void ExcluirConsulta()
+        public void DeletarConsulta(Consulta consulta)
         {
+            try
+            {
+                conexao.Open();
 
+                string sql = "exclude from consulta where id = @id";
+
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+                comando.Parameters.AddWithValue("@id", consulta.Id);
+
+                int linhasAfetadas = comando.ExecuteNonQuery();
+
+                if (linhasAfetadas > 0)
+                    MessageBox.Show("Consulta deletada com sucesso!");
+
+                else
+                    MessageBox.Show("Nenhum registro foi deletado. Verifique o ID informado.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
         }
         public List<Consulta> ListarConsultas()
         {
-            return new List<Consulta>();
+            var ListaConsultas = new List<Consulta>();
+
+            try
+            {
+                conexao.Open();
+
+                string sql = "select * from consulta";
+
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+                MySqlDataReader dados = comando.ExecuteReader();
+
+                while (dados.Read())
+                {
+                    Consulta consulta = new Consulta();
+                    consulta.Id = Convert.ToInt32(dados["Id"]);
+                    consulta.Tipo = dados["Tipo"].ToString();
+                    consulta.DataConsulta = Convert.ToDateTime(dados["DataConsulta"]);
+                    consulta.IdVeterinarioFk = Convert.ToInt32(dados["IdVeterinarioFk"]);
+                    consulta.IdAnimalFk = Convert.ToInt32(dados["IdAnimalFk"]);
+
+                    ListaConsultas.Add(consulta);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+            return ListaConsultas;
         }
     }
 }
