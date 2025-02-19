30a45b4ec562bf93f64e042e774fe27c30b81290
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-02-18 23:50:40 -0400

    refactor: VeterinarioContext

--- AppFormsVeterinario/Contexto/VeterinarioContext.cs
+++ AppFormsVeterinario/Contexto/VeterinarioContext.cs
@@ -17,54 +17,22 @@ namespace AppFormsVeterinario.Contexto
             conexao = new MySqlConnection(dados_conexao);
         }
 
-        public List<Veterinario> ListarVeterinarios()
-        {
-            List<Veterinario> listaVetParaExportar = new List<Veterinario>();// para retornar (return) o resutaldo e ser utilizado na aplicação
-            string sql = "SELECT * FROM VETERINARIO"; //consulta SQL para trazer todas as pessoas
-            try
-            {
-                MySqlCommand comando = new MySqlCommand(sql, conexao);//objeto "comando" responsável por ir até o banco e realizar ações
-                conexao.Open();//abrir a porta do banco para realizar a consulta
-                MySqlDataReader dados = comando.ExecuteReader(); //"comando" vai realizar a consulta e enviar tudo para dentro do objeto "dados"
-
-                //laço responsável por percorrer todos os registros que estão dentro do objeto "dados"
-                while (dados.Read())
-                {
-                    Veterinario veterinario = new Veterinario();
-                    veterinario.Id = Convert.ToInt32(dados["Id"]);
-                    veterinario.Nome = dados["Nome"].ToString();
-                    veterinario.Cpf = dados["Cpf"].ToString();
-                    veterinario.CRMV = dados["CRMV"].ToString();
-                    veterinario.Especializacao = dados["Especializacao"].ToString();
-                    listaVetParaExportar.Add(veterinario);
-                }
-                conexao.Close(); // Fechar a porta do banco após resultado da consulta         
-            }
-
-            catch (Exception ex)
-            {
-                MessageBox.Show("Erro: " + ex.Message);
-            }
-            return listaVetParaExportar; //retornar o resultado (exportar para aplicação)
-
-        }//fim do método para consultar e listar Pessoas (ListarPessoas)
-
         public void InserirVeterinario(Veterinario veterinario)
         {
-            string sql = "INSERT INTO VETERINARIO (Nome, Cpf, CRMV, Especializacao) VALUES (@Nome, @Cpf, @CRMV, @Especializacao)"; //para inserir uma pessoa no banco
-
             try
             {
+                conexao.Open();
+
+                string sql = "INSERT INTO VETERINARIO (Nome, Cpf, CRMV, Especializacao) VALUES (@Nome, @Cpf, @CRMV, @Especializacao)";
+
                 MySqlCommand comando = new MySqlCommand(sql, conexao);
 
-                // Adicionando parâmetros para evitar SQL Injection
                 comando.Parameters.AddWithValue("@Nome", veterinario.Nome);
                 comando.Parameters.AddWithValue("@Cpf", veterinario.Cpf);
                 comando.Parameters.AddWithValue("@CRMV", veterinario.CRMV);
                 comando.Parameters.AddWithValue("@Especializacao", veterinario.Especializacao);
 
-                conexao.Open(); // Abrir as portas do banco
-                int LinhasAfestadas = comando.ExecuteNonQuery(); //executa o comando e mostrar quantas linhas foran afetadas
+                comando.ExecuteNonQuery();
             }
 
             catch (Exception ex)
@@ -74,37 +42,31 @@ namespace AppFormsVeterinario.Contexto
 
             finally
             {
-                conexao.Close(); // Fecha as portas do banco, mesmo que ocorra erro
+                conexao.Close();
             }
-        } // fim do metodo para Inserir pessoas
-
+        }
         public void AtualizarVeterinario(Veterinario veterinario)
-        {            // Comando SQL para atualizar os dados da pessoa
+        {
             string sql = "UPDATE VETERINARIO SET Nome = @Nome, Cpf = @Cpf, CRMV = @CRMV, Especializacao = @Especializacao WHERE Id = @Id";
 
             try
             {
+                conexao.Open();
+
                 MySqlCommand comando = new MySqlCommand(sql, conexao);
 
-                // Adicionando parâmetros para evitar SQL Injection
                 comando.Parameters.AddWithValue("@Nome", veterinario.Nome);
                 comando.Parameters.AddWithValue("@Cpf", veterinario.Cpf);
                 comando.Parameters.AddWithValue("@CRMV", veterinario.CRMV);
                 comando.Parameters.AddWithValue("@Especializacao", veterinario.Especializacao);
                 comando.Parameters.AddWithValue("@Id", veterinario.Id);
 
-                conexao.Open(); // Abrir as portas do banco
-                int LinhasAfestadas = comando.ExecuteNonQuery(); // Executa o comando e retorna quantas linhas foran afetadas
+                int linhasAfetadas = comando.ExecuteNonQuery();
 
-                if (LinhasAfestadas > 0)
-                {
+                if (linhasAfetadas > 0)
                     MessageBox.Show("Veterinário atualizada com sucesso!");
-                }
-
                 else
-                {
                     MessageBox.Show("Nenhum registro foi atualizado. Verifique o ID informado.");
-                }
             }
 
             catch (Exception ex)
@@ -114,37 +76,32 @@ namespace AppFormsVeterinario.Contexto
 
             finally
             {
-                conexao.Close(); // Fecha a conexao com o banco
+                conexao.Close();
             }
-        } // fim do Atualizar pessoa
-
+        }
         public void DeletarVeterinario(Veterinario veterinario)
         {
-            // Comando SQL para atualizar os dados da pessoa
             string sql = "DELETE FROM VETERINARIO WHERE Id = @Id";
 
             try
             {
+                conexao.Open();
+
                 MySqlCommand comando = new MySqlCommand(sql, conexao);
 
-                // Adicionando parâmetros para evitar SQL Injection
                 comando.Parameters.AddWithValue("@Nome", veterinario.Nome);
                 comando.Parameters.AddWithValue("@Cpf", veterinario.Cpf);
                 comando.Parameters.AddWithValue("@CRMV", veterinario.CRMV);
                 comando.Parameters.AddWithValue("@Especializacao", veterinario.Especializacao);
                 comando.Parameters.AddWithValue("@Id", veterinario.Id);
 
-                conexao.Open(); // Abrir conexão com o banco
-                int linhasAfetadas = comando.ExecuteNonQuery(); // Executa o comando e retorna quantas linhas foram alteradas
+                int linhasAfetadas = comando.ExecuteNonQuery();
 
                 if (linhasAfetadas > 0)
-                {
                     MessageBox.Show("Veterinário deletada com sucesso!");
-                }
                 else
-                {
                     MessageBox.Show("Nenhum registro foi deletado. Verifique o ID informado.");
-                }
+                
             }
             catch (Exception ex)
             {
@@ -152,8 +109,43 @@ namespace AppFormsVeterinario.Contexto
             }
             finally
             {
-                conexao.Close(); // Fecha a conexão com o banco
+                conexao.Close();
             }
-        } //fim do Atualizar Pessoa
+        }
+        public List<Veterinario> ListarVeterinarios()
+        {
+            List<Veterinario> ListaVeterinarios = new List<Veterinario>();
+
+            try
+            {
+                conexao.Open();
+
+                string sql = "SELECT * FROM VETERINARIO";
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+                MySqlDataReader dados = comando.ExecuteReader();
+
+                while (dados.Read())
+                {
+                    Veterinario veterinario = new Veterinario();
+                    veterinario.Id = Convert.ToInt32(dados["Id"]);
+                    veterinario.Nome = dados["Nome"].ToString();
+                    veterinario.Cpf = dados["Cpf"].ToString();
+                    veterinario.CRMV = dados["CRMV"].ToString();
+                    veterinario.Especializacao = dados["Especializacao"].ToString();
+                    ListaVeterinarios.Add(veterinario);
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+            return ListaVeterinarios;
+        }
     }
 }
