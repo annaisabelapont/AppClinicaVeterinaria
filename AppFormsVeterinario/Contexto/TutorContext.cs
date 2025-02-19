c5ed6e9207893167cb693450f061d18ade96c533
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-02-18 23:51:19 -0400

    feat: TutorContext

--- AppFormsVeterinario/Contexto/TutorContext.cs
+++ AppFormsVeterinario/Contexto/TutorContext.cs
@@ -17,7 +17,7 @@ namespace AppFormsVeterinario.Contexto
             conexao = new MySqlConnection(dados_conexao);
         }
 
-        public void CadastrarTutor(Tutor tutor)
+        public void InserirTutor(Tutor tutor)
         {
             try
             {
@@ -43,20 +43,101 @@ namespace AppFormsVeterinario.Contexto
                 conexao.Close();
             }
         }
-
-        public void EditarTutor(Tutor tutor)
+        public void AtualizarTutor(Tutor tutor)
         {
+            try
+            {
+                conexao.Open();
 
+                string sql = "update tutor set nome = @nome, telefone = @telefone, cpf = @cpf, cep = @cep where id = @id";
+
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+                comando.Parameters.AddWithValue("@nome", tutor.Nome);
+                comando.Parameters.AddWithValue("@telefone", tutor.Telefone);
+                comando.Parameters.AddWithValue("@cpf", tutor.Cpf);
+                comando.Parameters.AddWithValue("@cep", tutor.Cep);
+
+                int linhasAfetadas = comando.ExecuteNonQuery();
+
+                if (linhasAfetadas > 0)
+                    MessageBox.Show("Tutor atualizado com sucesso!");
+                else
+                    MessageBox.Show("Nenhum registro foi atualizado. Verifique o ID informado.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
         }
-
-        public void ExcluirTutor(Tutor tutor)
+        public void DeletarTutor(Tutor tutor)
         {
+            try
+            {
+                conexao.Open();
 
+                string sql = "exclude from tutor where id = @id";
+
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+                comando.Parameters.AddWithValue("@id", tutor.Id);
+
+                int linhasAfetadas = comando.ExecuteNonQuery();
+
+                if (linhasAfetadas > 0)
+                    MessageBox.Show("Tutor deletado com sucesso!");
+                else
+                    MessageBox.Show("Nenhum registro foi deletado. Verifique o ID informado.");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
         }
-
         public List<Tutor> ListarTutores()
         {
-            return new List<Tutor>();
+            var ListaTutores = new List<Tutor>();
+
+            try
+            {
+                conexao.Open();
+
+                string sql = "select * from tutor";
+
+                MySqlCommand comando = new MySqlCommand(sql, conexao);
+
+                MySqlDataReader dados = comando.ExecuteReader();
+
+                while (dados.Read())
+                {
+                    Tutor tutor = new Tutor();
+                    tutor.Id = Convert.ToInt32(dados["Id"]);
+                    tutor.Nome = dados["Nome"].ToString();
+                    tutor.Telefone = dados["Telefone"].ToString();
+                    tutor.Cpf = dados["Cpf"].ToString();
+                    tutor.Cep = dados["Cep"].ToString();
+
+                    ListaTutores.Add(tutor);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+            finally
+            {
+                conexao.Close();
+            }
+
+            return ListaTutores;
         }
     }
 }
