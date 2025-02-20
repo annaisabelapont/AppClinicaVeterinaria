f9058b00af47aca55c86f81b4c6bc33b37275da9
Anna Isabela Pontuschka <annaisabelapont@gmail.com>
2025-02-19 22:28:43 -0400

    feat: ConsultaVet delete

--- AppFormsVeterinario/Formularios/ConsultaVetForm/FormDeletarConsultaVet.cs
+++ AppFormsVeterinario/Formularios/ConsultaVetForm/FormDeletarConsultaVet.cs
@@ -1,20 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
+﻿using AppFormsVeterinario.Contexto;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using AppFormsVeterinario.Models;
 
 namespace AppFormsVeterinario.Formularios.ConsultaVetForm
 {
     public partial class FormDeletarConsultaVet : Form
     {
+        ConsultaVetContext consultaContext = new ConsultaVetContext();
+        Consulta consultaSelecionada = new Consulta();
         public FormDeletarConsultaVet()
         {
             InitializeComponent();
+            cbConsulta.DataSource = consultaContext.ListarConsultas().ToList();
+            cbConsulta.DisplayMember = "Tipo";
+            cbConsulta.SelectedIndex = -1;
+        }
+
+        private void cbConsulta_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indexSelec = cbConsulta.SelectedIndex;
+            if (indexSelec > -1)
+            {
+                Consulta consulta = consultaContext.ListarConsultas()[indexSelec];
+                //preencher txts
+                consultaSelecionada = consulta;
+            }
+        }
+
+        private void btExcluir_Click(object sender, EventArgs e)
+        {
+            consultaContext.DeletarConsulta(consultaSelecionada);
+            //limpar txts e cb
         }
     }
 }
